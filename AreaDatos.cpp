ff03f1955e32cea762a44c4bba8eba2641b76047
tomas schlotahuer <tomsschlotahuer@hotmail.com>
2025-03-21 22:41:03 -0300

    cositas

--- AreaDatos.cpp
+++ AreaDatos.cpp
@@ -11,7 +11,7 @@ AreaDatos::AreaDatos(int ELM_POR_BLOQ, int OMAX, int PMAX){
     this->registros.reserve(OMAX);
 }
 
-int AreaDatos::insertarRegistro(int clave, string dato)
+int AreaDatos::insertarRegistro(int pos, int clave, string dato)
 {
     /*  Casos posibles:
         1: No bloques, creamos un bloque e insertamos el elemento.
@@ -20,18 +20,51 @@ int AreaDatos::insertarRegistro(int clave, string dato)
         3: Insercion en el ultimo bloque estando n/2 lleno
         4: Insercion al final del overflow (sin espacio)
         5: Insercion genera el ultimo bloque disponible a crear en el area primaria.
+
+        Codigos: 
+        1: Insercion usando nuevo bloque
+        2: Insercion con overflow lleno
+        3: Insercion con area primaria llena
     */
+   int lugarInsercion = 0;
+
    if (!CANTIDAD_BLOQUES) {
         this->crearBloque(0);
         this->registros[0] = {clave, dato, 0}; // Por default 0 indica que no apunta al overflow.
         return 1;
+    }
+
+    if (!this->isLastBlock(pos)){
+        if (!this->isBlockFull(pos)) {
+
+        } // caso bloque lleno
+
+
     } 
+
+    // caso ultimo bloque
+    // falta manejar el caso de sin espacio para insertar nuevos bloques   
+
+
 
 }
 
 void AreaDatos::crearBloque(int pos)
 {
     this->CANTIDAD_BLOQUES++;
+    this->ultimoBloqueInsertado = pos;
+}
+
+bool AreaDatos::isBlockFull(int pos)
+{
+    for (int x = pos; x < pos+this->ELM_POR_BLOQ; x++)
+        if (this->registros[x].clave == 0) return false;
+    return true;
+}
+
+bool AreaDatos::isLastBlock(int pos)
+{
+    return pos == this->ultimoBloqueInsertado;
 }
 
 #endif // AREA_DATOS_CPP
--- AreaDatos.h
+++ AreaDatos.h
@@ -9,16 +9,18 @@ using namespace std;
 class AreaDatos{
     public:
         AreaDatos(int ELM_POR_BLOQ, int OMAX, int PMAX);
-        int insertar(int clave, string dato);
-        string* consultar(int clave); // string luego se reemplaza por Template
+        int insertarRegistro(int pos, int clave, string dato);
+        string* obtenerRegistro(int clave); // string luego se reemplaza por Template
         vector<Indice> obtenerTablaIndices();
         friend ostream& operator<< (ostream& os, AreaDatos& areaDatos);
-        int getCantidadRegistros();
     private:
         vector<Registro> registros;
         int CANTIDAD_BLOQUES = 0, ELM_POR_BLOQ, OMAX, PMAX;    // ELM_POR_BLOQ = n; OMAX = tamanio maximo array; PMAX = tamanio maximo zona de datos
+        int ultimoBloqueInsertado;
         void ordenarBloque(int posInit);
-        void crearBloque();
+        void crearBloque(int pos);
+        bool checkBloqueLleno(int pos);
+        bool IsLastBlock(int pos);
 };
 
 #endif // !AREA_DATOS_H
